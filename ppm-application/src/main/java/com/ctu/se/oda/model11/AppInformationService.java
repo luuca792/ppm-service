4a536f13dc58087fb5f78042b0753d8b6b9dad8a
luuca792 <luuca792@gmail.com>
2023-10-31 21:23:49 +0700

    Enhance logic and remove unused dependency

--- ppm-application/src/main/java/com/ctu/se/oda/model11/AppInformationService.java
+++ ppm-application/src/main/java/com/ctu/se/oda/model11/AppInformationService.java
@@ -3,13 +3,17 @@ package com.ctu.se.oda.model11;
 import com.ctu.se.oda.model11.models.AppInfo;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class AppInformationService {
 
     private static AppInfo appInfo;
 
     public static AppInfo getAppInfo() {
-        appInfo = new AppInfo("Production Process Management Service", "0.1.0");
+        if (Objects.isNull(appInfo)) {
+            appInfo = new AppInfo("Production Process Management Service", "0.1.0");
+        }
         return appInfo;
     }
 }
