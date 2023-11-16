0127743d1c4c3ba3112bafdd75f2c4a958c713fb
anhy167 <ttka1607@gmail.com>
2023-11-16 21:49:49 +0700

    PPM_0009: User service

--- ppm-infrastructure/src/main/java/com/ctu/se/oda/model11/daos/UserDAO.java
+++ ppm-infrastructure/src/main/java/com/ctu/se/oda/model11/daos/UserDAO.java
@@ -3,12 +3,13 @@ package com.ctu.se.oda.model11.daos;
 import com.ctu.se.oda.model11.entities.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,9 +17,14 @@ import java.util.List;
 @NoArgsConstructor
 @Validated
 public class UserDAO {
-    public List<User> getUsersFromFile(String filePath) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(filePath)));
-        ObjectMapper objectMapper = new ObjectMapper();
-        return Arrays.asList(objectMapper.readValue(content, User[].class));
+    public List<User> getUsersFromFile() {
+        try {
+            Resource resource = new ClassPathResource("data/user.txt");
+            InputStream inputStream = resource.getInputStream();
+            ObjectMapper objectMapper = new ObjectMapper();
+            return Arrays.asList(objectMapper.readValue(inputStream, User[].class));
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading user file", e);
+        }
     }
 }
--- ppm-main/src/main/java/com/ctu/se/oda/model11/api/UserApi.java
+++ ppm-main/src/main/java/com/ctu/se/oda/model11/api/UserApi.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
 
@@ -19,27 +18,21 @@ public class UserApi {
 
     @GetMapping("/login")
     public ResponseEntity<?> login(@RequestHeader(name = "Authorization") String authorizationHeader) {
-        try {
-            String base64Credentials = authorizationHeader.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded);
+        String base64Credentials = authorizationHeader.substring("Basic".length()).trim();
+        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+        String credentials = new String(credDecoded);
 
-            // Tách tên người dùng và mật khẩu từ thông tin xác thực giải mã
-            final String[] values = credentials.split(":", 2);
-            String username = values[0];
+        final String[] values = credentials.split(":", 2);
+        String username = values[0];
 
-            String fileUser = "E:\\LuanVan\\Code\\ppm-service-0.1.0\\ppm-service\\ppm-main\\src\\main\\java\\com\\ctu\\se\\oda\\model11\\data\\user.txt";
-            List<User> users = userDAO.getUsersFromFile(fileUser);
+        List<User> users = userDAO.getUsersFromFile();
 
-            for (User user : users) {
-                if (user.getUserInfo().getUsername().equals(username)) {
-                    return new ResponseEntity<>(user, HttpStatus.OK);
-                }
+        for (User user : users) {
+            if (user.getUserInfo().getUsername().equals(username)) {
+                return new ResponseEntity<>(user, HttpStatus.OK);
             }
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
 }
